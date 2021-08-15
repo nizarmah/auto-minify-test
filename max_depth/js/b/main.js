10688ffa57d5e1f8d80fe16d2560d60b908d76ad
nizarmah <nizarmah@users.noreply.github.com>
2021-08-15 14:03:59 +0000

    Github Action: Auto Minified JS and CSS files

--- max_depth/js/b/main.js
+++ max_depth/js/b/main.js
@@ -1,7 +1,1 @@
-function print_hello() {
-	console.log('hello world');
-}
-
-window.addEventListener('load', function () {
-	print_hello();
-});
+function print_hello(){console.log("hello world")}window.addEventListener("load",function(){print_hello()});
